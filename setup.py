941518c6add607509a43ec302f6496d8502b1aca
Leo Cances <leocances@gmail.com>
2024-04-10 16:14:23 +0200

    update setup url

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name='deropy',
     version=os.getenv('DEROPY_VERSION') or '0.0.1',
-    url='https://github.com/lcances/pydero',
+    url='https://github.com/lcances/deropy',
     author_email='leocances@gmail.com',
     description='A set of tool to help of DERO smart contract development',
     packages=find_packages(),
