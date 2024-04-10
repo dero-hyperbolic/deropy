218cc08c64e02599183f87ecc463194ec2a47b31
Leo Cances <leocances@gmail.com>
2024-04-10 20:19:14 +0200

    show help when calling deropy without any command

--- deropy/main.py
+++ deropy/main.py
@@ -9,7 +9,10 @@ from deropy.commands.deploy import deploy
 @click.option('--verbose', '-v', is_flag=True, help='Enable verbose logging')
 def deropy(verbose: bool):
     """deropy is a CLI for deropy."""
-    pass
+    
+    # if no arguments are passed, show the help message
+    if not any([verbose]):
+        click.echo(deropy.get_help(click.Context(deropy)))
 
 deropy.add_command(generate)
 deropy.add_command(deploy)
