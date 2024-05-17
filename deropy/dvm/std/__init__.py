bb946a156c444ddb1189595da3aeb721f3304e2f
Leo Cances <leocances@gmail.com>
2024-05-17 21:15:59 +0200

    ClaimOwnership and TransferOwnership added to std

--- deropy/dvm/std/__init__.py
+++ deropy/dvm/std/__init__.py
@@ -1,6 +1,8 @@
 from .updateCode import updateCode
 from .increment import increment
 from .decrement import decrement
+from .claim_ownership import ClaimOwnership
+from .transfer_ownership import TransferOwnership
 
 
-__all__ = ['updateCode', 'increment', 'decrement']
+__all__ = ['updateCode', 'increment', 'decrement', 'ClaimOwnership', 'TransferOwnership']
